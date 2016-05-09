7e3c8504467e2bd864775fb65334b9b35b58e550
Adam Coulon <adamgcoulon@gmail.com>
2016-05-08 23:16:47 -0500

    updated radio url

--- radio/services.js
+++ radio/services.js
@@ -3,7 +3,7 @@ angular.module('CoffeeHouseCoworkers.radio.services', [])
 .factory('RadioService', ['$http', function($http) {
 	return {
 		getLive: function(){
-			return $http.get('http://192.241.169.148:4000/live');
+			return $http.get('http://192.241.169.148:8090/radio/now');
 		}
 	};
 }]);
